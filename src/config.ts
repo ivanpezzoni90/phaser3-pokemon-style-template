6d8d4363e15542b406d1c59427cbf1921c9e7ade
Ivan Pezzoni <ivan.pezzoni@immobiliare.it>
2022-03-21 12:45:13 +0100

    added eslint, changed sprite

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,15 @@
 import Phaser from 'phaser';
 
 export default {
-  type: Phaser.AUTO,
-  parent: 'game',
-  backgroundColor: '#33A5E7',
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: { y: 0 },
-        debug: false
-    }
-  },
-  scale: {
+    type: Phaser.AUTO,
     width: 800,
     height: 600,
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    parent: 'game',
+    backgroundColor: '#33A5E7',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+        }
+    }
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,139 +1,172 @@
 import Phaser from 'phaser';
 
 export default class Demo extends Phaser.Scene {
-  platforms: any;
-  player: any;
-  cursors: any;
-  stars: any;
-  bombs: any;
-  scoreText: any;
-  score: any = 0;
+    platforms: any;
+    player: any;
+    cursors: any;
+    stars: any;
+    bombs: any;
+    scoreText: any;
+    score: any = 0;
 
-  constructor() {
-    super('GameScene');
-  }
+    constructor() {
+        super('GameScene');
+    }
 
-  preload() {
-    this.load.image("tiles", "../assets/tuxmon-sample-32px-extruded.png");
-    this.load.tilemapTiledJSON("map", "../assets/tuxemon-town.json");
+    preload() {
+        this.load.image('tiles', '../tilesets/tuxmon-sample-32px-extruded.png');
+        this.load.tilemapTiledJSON('map', '../assets/tuxemon-town.json');
 
-    this.load.spritesheet('warrior', 
-        'assets/warriorArraySprite.png',
-        { frameWidth: 16, frameHeight: 18 }
-    );
-  }
+        // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
+        // the player animations (walking left, walking right, etc.) in one image. For more info see:
+        //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
+        // If you don't use an atlas, you can do the same thing with a spritesheet, see:
+        //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
+        this.load.atlas('atlas', '../atlas/atlas.png', '../atlas/atlas.json');
+    }
 
-  create() {
-    const map = this.make.tilemap({ key: "map" });
+    create() {
 
-    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+        const map = this.make.tilemap({ key: 'map' });
+  
+        const tileset = map.addTilesetImage('tuxmon-sample-32px-extruded', 'tiles');
+  
+        const belowLayer = map.createLayer('Below Player', tileset, 0, 0);
+        const worldLayer = map.createLayer('World', tileset, 0, 0);
+        const aboveLayer = map.createLayer('Above Player', tileset, 0, 0);
+  
+        worldLayer.setCollisionByProperty({ collides: true });
+  
+        aboveLayer.setDepth(10);
+  
+        const spawnPoint: any = map.findObject('Objects', (obj) => obj.name === 'Spawn Point');
+  
+        this.player = this.physics.add
+            .sprite(spawnPoint.x, spawnPoint.y, 'atlas', 'misa-front')
+            .setSize(30, 40)
+            .setOffset(0, 24);
+  
+        this.physics.add.collider(this.player, worldLayer);
+  
+        const anims = this.anims;
+        anims.create({
+            key: 'misa-left-walk',
+            frames: anims.generateFrameNames('atlas', {
+                prefix: 'misa-left-walk.',
+                start: 0,
+                end: 3,
+                zeroPad: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        anims.create({
+            key: 'misa-right-walk',
+            frames: anims.generateFrameNames('atlas', {
+                prefix: 'misa-right-walk.',
+                start: 0,
+                end: 3,
+                zeroPad: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        anims.create({
+            key: 'misa-front-walk',
+            frames: anims.generateFrameNames('atlas', {
+                prefix: 'misa-front-walk.',
+                start: 0,
+                end: 3,
+                zeroPad: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        anims.create({
+            key: 'misa-back-walk',
+            frames: anims.generateFrameNames('atlas', {
+                prefix: 'misa-back-walk.',
+                start: 0,
+                end: 3,
+                zeroPad: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+  
 
-    const belowLayer = map.createLayer("Below Player", tileset, 0, 0);
-    const worldLayer = map.createLayer("World", tileset, 0, 0);
-    const aboveLayer = map.createLayer("Above Player", tileset, 0, 0);
+        const camera = this.cameras.main;
+        camera.startFollow(this.player);
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-    worldLayer.setCollisionByProperty({ collides: true });
-    aboveLayer.setDepth(10);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.player = this.physics.add.sprite(100, 450, 'warrior');
-    this.player.setScale(2);
+        // Help text that has a "fixed" position on the screen
+        this.add
+            .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+                font: '18px monospace',
+                padding: { x: 20, y: 10 },
+                backgroundColor: '#ffffff',
+            })
+            .setScrollFactor(0)
+            .setDepth(30);
+ 
+        // Debug graphics
+        this.input.keyboard.once('keydown-D', (event: any) => {
+            // Turn on physics debugging to show player's hitbox
+            this.physics.world.createDebugGraphic();
+ 
+            // Create worldLayer collision graphic above the player, but below the help text
+            const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
+            worldLayer.renderDebug(graphics, {
+                tileColor: null, // Color of non-colliding tiles
+                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+                faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+            });
+        });
 
-    this.player.setCollideWorldBounds(true);
-    this.physics.add.collider(this.player, worldLayer);
-
-    this.anims.create({
-        key: 'up',
-        frames: this.anims.generateFrameNumbers('warrior', { start: 0, end: 2 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    
-    this.anims.create({
-        key: 'idle-down',
-        frames: [ { key: 'warrior', frame: 6 } ],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'idle-up',
-        frames: [ { key: 'warrior', frame: 1 } ],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'idle-left',
-        frames: [ { key: 'warrior', frame: 9 } ],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'idle-right',
-        frames: [ { key: 'warrior', frame: 5 } ],
-        frameRate: 20
-    });
-    
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('warrior', { start: 3, end: 5 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('warrior', { start: 6, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('warrior', { start: 9, end: 11 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    const camera = this.cameras.main;
-    camera.startFollow(this.player);
-    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  }
+    }
 
     update(time: number, delta: number): void {
-        this.cursors = this.input.keyboard.createCursorKeys();
         const speed = 175;
         const prevVelocity = this.player.body.velocity.clone();
-
+    
         // Stop any previous movement from the last frame
         this.player.body.setVelocity(0);
-        this.player.body.velocity.normalize().scale(0.8);
-
-            // Horizontal movement
+        // Horizontal movement
         if (this.cursors.left.isDown) {
             this.player.body.setVelocityX(-speed);
         } else if (this.cursors.right.isDown) {
             this.player.body.setVelocityX(speed);
         }
+        
         // Vertical movement
         if (this.cursors.up.isDown) {
             this.player.body.setVelocityY(-speed);
         } else if (this.cursors.down.isDown) {
             this.player.body.setVelocityY(speed);
         }
-
+        
+        // Normalize and scale the velocity so that this.player can't move faster along a diagonal
         this.player.body.velocity.normalize().scale(speed);
-
+        
+        // Update the animation last and give left/right animations precedence over up/down animations
         if (this.cursors.left.isDown) {
-            this.player.anims.play("left", true);
-          } else if (this.cursors.right.isDown) {
-            this.player.anims.play("right", true);
-          } else if (this.cursors.up.isDown) {
-            this.player.anims.play("up", true);
-          } else if (this.cursors.down.isDown) {
-            this.player.anims.play("down", true);
-          } else {
+            this.player.anims.play('misa-left-walk', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.anims.play('misa-right-walk', true);
+        } else if (this.cursors.up.isDown) {
+            this.player.anims.play('misa-back-walk', true);
+        } else if (this.cursors.down.isDown) {
+            this.player.anims.play('misa-front-walk', true);
+        } else {
+            this.player.anims.stop();
+        
             // If we were moving, pick and idle frame to use
-            if (prevVelocity.x < 0) this.player.anims.play("idle-left", true);
-            else if (prevVelocity.x > 0) this.player.anims.play("idle-right", true);
-            else if (prevVelocity.y < 0) this.player.anims.play("idle-up", true);
-            else if (prevVelocity.y > 0) this.player.anims.play("idle-down", true);
-          }
-
+            if (prevVelocity.x < 0) this.player.setTexture('atlas', 'misa-left');
+            else if (prevVelocity.x > 0) this.player.setTexture('atlas', 'misa-right');
+            else if (prevVelocity.y < 0) this.player.setTexture('atlas', 'misa-back');
+            else if (prevVelocity.y > 0) this.player.setTexture('atlas', 'misa-front');
+        }
     }
 }
