714e5373f08f574abc591a1bb50cf845ca749296
Ivan Pezzoni <ivan.pezzoni@immobiliare.it>
2022-03-22 10:17:27 +0100

    added correct sprite

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -17,12 +17,10 @@ export default class Demo extends Phaser.Scene {
         this.load.image('tiles', '../tilesets/tuxmon-sample-32px-extruded.png');
         this.load.tilemapTiledJSON('map', '../assets/tuxemon-town.json');
 
-        // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
-        // the player animations (walking left, walking right, etc.) in one image. For more info see:
-        //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
-        // If you don't use an atlas, you can do the same thing with a spritesheet, see:
-        //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
-        this.load.atlas('atlas', '../atlas/atlas.png', '../atlas/atlas.json');
+        this.load.spritesheet('girl', 
+            'assets/amelia.png',
+            { frameWidth: 16, frameHeight: 24 }
+        );
     }
 
     create() {
@@ -41,90 +39,90 @@ export default class Demo extends Phaser.Scene {
   
         const spawnPoint: any = map.findObject('Objects', (obj) => obj.name === 'Spawn Point');
   
-        this.player = this.physics.add
-            .sprite(spawnPoint.x, spawnPoint.y, 'atlas', 'misa-front')
-            .setSize(30, 40)
-            .setOffset(0, 24);
-  
-        this.physics.add.collider(this.player, worldLayer);
-  
-        const anims = this.anims;
-        anims.create({
-            key: 'misa-left-walk',
-            frames: anims.generateFrameNames('atlas', {
-                prefix: 'misa-left-walk.',
-                start: 0,
-                end: 3,
-                zeroPad: 3,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-        anims.create({
-            key: 'misa-right-walk',
-            frames: anims.generateFrameNames('atlas', {
-                prefix: 'misa-right-walk.',
-                start: 0,
-                end: 3,
-                zeroPad: 3,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-        anims.create({
-            key: 'misa-front-walk',
-            frames: anims.generateFrameNames('atlas', {
-                prefix: 'misa-front-walk.',
-                start: 0,
-                end: 3,
-                zeroPad: 3,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-        anims.create({
-            key: 'misa-back-walk',
-            frames: anims.generateFrameNames('atlas', {
-                prefix: 'misa-back-walk.',
-                start: 0,
-                end: 3,
-                zeroPad: 3,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-  
+        this.player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'girl')
+            .setScale(1.5);
 
+        this.physics.add.collider(this.player, worldLayer);
+
+        // Create animations
+        this.anims.create({
+            key: 'idle-right',
+            frames: this.anims.generateFrameNumbers('girl', { start: 0, end: 5 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'idle-up',
+            frames: this.anims.generateFrameNumbers('girl', { start: 6, end: 11 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'idle-left',
+            frames: this.anims.generateFrameNumbers('girl', { start: 12, end: 17 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'idle-down',
+            frames: this.anims.generateFrameNumbers('girl', { start: 18, end: 23 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'walk-right',
+            frames: this.anims.generateFrameNumbers('girl', { start: 24, end: 29 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'walk-up',
+            frames: this.anims.generateFrameNumbers('girl', { start: 30, end: 35 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'walk-left',
+            frames: this.anims.generateFrameNumbers('girl', { start: 36, end: 41 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'walk-down',
+            frames: this.anims.generateFrameNumbers('girl', { start: 42, end: 47 }),
+            frameRate: 10,
+            repeat: -1
+        });
+  
         const camera = this.cameras.main;
         camera.startFollow(this.player);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        // Help text that has a "fixed" position on the screen
-        this.add
-            .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
-                font: '18px monospace',
-                padding: { x: 20, y: 10 },
-                backgroundColor: '#ffffff',
-            })
-            .setScrollFactor(0)
-            .setDepth(30);
+        // Enable debug text when needed
+        // this.add
+        //     .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+        //         font: '18px monospace',
+        //         padding: { x: 20, y: 10 },
+        //         backgroundColor: '#666',
+        //     })
+        //     .setScrollFactor(0)
+        //     .setDepth(30);
  
-        // Debug graphics
-        this.input.keyboard.once('keydown-D', (event: any) => {
-            // Turn on physics debugging to show player's hitbox
-            this.physics.world.createDebugGraphic();
+        // // Debug graphics
+        // this.input.keyboard.once('keydown-D', (event: any) => {
+        //     // Turn on physics debugging to show player's hitbox
+        //     this.physics.world.createDebugGraphic();
  
-            // Create worldLayer collision graphic above the player, but below the help text
-            const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
-            worldLayer.renderDebug(graphics, {
-                tileColor: null, // Color of non-colliding tiles
-                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-                faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
-            });
-        });
-
+        //     // Create worldLayer collision graphic above the player, but below the help text
+        //     const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
+        //     worldLayer.renderDebug(graphics, {
+        //         tileColor: null, // Color of non-colliding tiles
+        //         collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        //         faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+        //     });
+        // });
     }
 
     update(time: number, delta: number): void {
@@ -139,34 +137,29 @@ export default class Demo extends Phaser.Scene {
         } else if (this.cursors.right.isDown) {
             this.player.body.setVelocityX(speed);
         }
-        
         // Vertical movement
         if (this.cursors.up.isDown) {
             this.player.body.setVelocityY(-speed);
         } else if (this.cursors.down.isDown) {
             this.player.body.setVelocityY(speed);
         }
-        
-        // Normalize and scale the velocity so that this.player can't move faster along a diagonal
+
         this.player.body.velocity.normalize().scale(speed);
-        
-        // Update the animation last and give left/right animations precedence over up/down animations
+
         if (this.cursors.left.isDown) {
-            this.player.anims.play('misa-left-walk', true);
+            this.player.anims.play('walk-left', true);
         } else if (this.cursors.right.isDown) {
-            this.player.anims.play('misa-right-walk', true);
+            this.player.anims.play('walk-right', true);
         } else if (this.cursors.up.isDown) {
-            this.player.anims.play('misa-back-walk', true);
+            this.player.anims.play('walk-up', true);
         } else if (this.cursors.down.isDown) {
-            this.player.anims.play('misa-front-walk', true);
+            this.player.anims.play('walk-down', true);
         } else {
-            this.player.anims.stop();
-        
             // If we were moving, pick and idle frame to use
-            if (prevVelocity.x < 0) this.player.setTexture('atlas', 'misa-left');
-            else if (prevVelocity.x > 0) this.player.setTexture('atlas', 'misa-right');
-            else if (prevVelocity.y < 0) this.player.setTexture('atlas', 'misa-back');
-            else if (prevVelocity.y > 0) this.player.setTexture('atlas', 'misa-front');
+            if (prevVelocity.x < 0) this.player.anims.play('idle-left', true);
+            else if (prevVelocity.x > 0) this.player.anims.play('idle-right', true);
+            else if (prevVelocity.y < 0) this.player.anims.play('idle-up', true);
+            else if (prevVelocity.y > 0) this.player.anims.play('idle-down', true);
         }
     }
 }
